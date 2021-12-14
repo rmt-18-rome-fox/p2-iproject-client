4241adac1690c206e1a82b6c9129ef53bbe2a4ba
ihzaw <ihzanantama@gmail.com>
2021-12-14 23:24:13 +0800

    setup vue-cli + mapbox setup

---
+++ carbon-delivery/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-[461111]">Hello World</h1>
+    <div>
+    <Map class="w-full" />
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Map from './components/Map.vue'
+export default {
+  components: {
+    Map 
+  },
+  
+}
+</script>
+
+<style>
+/* #map {
+  height: 100vh;
+  width: 100vw
+} */
+</style>
---
+++ carbon-delivery/src/components/Map.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="map" class="w-96 h-96"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  name: "Map",
+  methods: {
+    loadMap() {
+      navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+        enableHighAccuracy: true,
+      });
+
+      function successLocation(position) {
+        setupMap([position.coords.longitude, position.coords.latitude]);
+      }
+      function errorLocation() {}
+
+      function setupMap(center) {
+        // center = [longitude, latitude];
+        const access_token =
+          "pk.eyJ1IjoiaWh6YW5hbnRhbWEiLCJhIjoiY2t4NW02Y3B3MDNhajJ2bzNtZzllcmhyYiJ9.keA6mU1OUSS8JqO9U9n8hg";
+        mapboxgl.accessToken = access_token;
+
+        const map = new mapboxgl.Map({
+          container: "map",
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: center,
+          zoom: 15,
+        });
+
+        const MapboxDirections = window.MapboxDirections;
+        const directions = new MapboxDirections({
+          accessToken: access_token,
+          unit: "metric",
+          profile: 'mapbox/driving',
+          controls: {
+            profileSwitcher: false
+          },
+          placeholderOrigin: 'Where does your shipping starts?',
+          placeholderDestination: 'Where do the courier drop your stuff?',
+
+        });
+        map.addControl(directions, "top-left");
+        directions.on('origin', () => {
+            const originCoord = directions.getOrigin().geometry.coordinates
+            console.log("ini origin coord :", originCoord)
+        })
+        directions.on('destination', () => {
+            const destinationCoord = directions.getDestination().geometry.coordinates
+            console.log("ini dest COord :", destinationCoord)
+        })
+      }
+    },
+  },
+  mounted() {
+    this.loadMap();
+  },
+};
+</script>
+
+<style></style>
