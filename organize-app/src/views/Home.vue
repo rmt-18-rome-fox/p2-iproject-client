a21b7fc916e43c0db3a1e72175f33ce5d1435b7f
luthfanadli <luthfan30@gmail.com>
2021-12-14 20:46:08 +0700

    login register client on progress

--- organize-app/src/views/Home.vue
+++ organize-app/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-9">
+        </div>
+        <div class="col-lg-9 col-md-6">
+          <div class="row">
+            <div class="=col-md-4 my-3">
+              <task-card> </task-card>
+              <div class="col-lg-4 col-md-10">
+                <div class="card-body d-grid"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TaskCard from "../components/TaskCard.vue";
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    TaskCard,
+  },
+};
 </script>
+
+<style>
+</style>
