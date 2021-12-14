a6d69b498311698dd179c6d8e62123d4062dfce9
siska <siska25.oktaviani@gmail.com>
2021-12-14 16:28:48 +0700

    initial template

---
+++ book-collector/src/components/RegisterPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="registerForm">
+    <b-form>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Your Password:" label-for="input-3">
+        <b-form-input
+          type="password"
+          id="input-3"
+          placeholder="Enter Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.registerForm{
+    margin: 50px;
+    width: 50vh;
+}
+</style>
