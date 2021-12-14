2dabdac87026138dda0ae3dd8b1b93550be11e8e
Whisnubudih <whisnubudih@gmail.com>
2021-12-15 02:03:54 +0700

    add view and component + template

--- project-superhero-fanbase/src/App.vue
+++ project-superhero-fanbase/src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+       <Navbar></Navbar>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
     <router-view/>
   </div>
 </template>
 
+
+     
+    },
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  name: "app",
+  components: {
+      Navbar,
+     
+    },
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ project-superhero-fanbase/src/views/Register.vue
@@ -0,0 +1,43 @@
+<template>
+     <div class="simple-login-container">
+        <form  action="">
+            <h2>LOGIN</h2>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <input   type="text" class="form-control" placeholder="Username">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <input  type="email" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <input type="password" placeholder="Password" class="form-control">
+                </div>
+            </div>
+             <div class="row">
+                <div class="col-md-12 form-group">
+                    <input   type="text" class="form-control" placeholder="Phone Number">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <input type="submit" class="btn btn-block btn-login" placeholder="Enter your Password" >
+                </div>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Register"
+}
+</script>
+
+<style>
+
+</style>
