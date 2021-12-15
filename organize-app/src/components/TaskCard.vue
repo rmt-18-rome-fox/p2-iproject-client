25f67cad0e0b6db25e7c457e36ca2a93bae225cb
luthfanadli <luthfan30@gmail.com>
2021-12-15 15:43:01 +0700

    delete task

--- organize-app/src/components/TaskCard.vue
+++ organize-app/src/components/TaskCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mt-5">
-      <!-- <h5>TES</h5> -->
+    <!-- <h5>TES</h5> -->
     <div class="row row-cols-4 d-flex justify-content-between">
       <div v-for="task in tasks" :key="task.id" class="col">
         <div class="card">
@@ -9,7 +9,19 @@
             <h5 class="card-title">{{ task.title }}</h5>
             <h5 class="card-category">Category: {{ task.Category.name }}</h5>
             <!-- <h6 class="card-description">Description: {{ task.description }}</h6> -->
-            <button @click.prevent="getTaskDetail(task.id)" class="btn btn-primary my-2">Tasks Details</button>
+            <button
+              @click.prevent="getTaskDetail(task.id)"
+              class="btn btn-primary my-2"
+            >
+              Task Details
+            </button>
+            <button
+              @click.prevent="deleteTask(task.id)"
+              type="button"
+              class="my-2 btn btn-danger mb-2"
+            >
+              Delete Task
+            </button>
           </div>
         </div>
       </div>
@@ -19,24 +31,30 @@
 
 <script>
 export default {
-    name: "TaskCard",
-    computed: {
-        tasks(){
-            console.log({data:this.$store.state.tasks});
-            return this.$store.state.tasks
-        }
+  name: "TaskCard",
+  computed: {
+    tasks() {
+      // console.log({data:this.$store.state.tasks});
+      return this.$store.state.tasks;
     },
-    methods: {
-        getTaskDetail(detailId){
-            this.$store.dispatch('getProductDetail', detailId).then(()=>{
-                this.$router.push(`/task/${detailId}`)
-            })
-        }
+  },
+  methods: {
+    getTaskDetail(detailId) {
+    //   console.log(detailId, "<<<detailId");
+      this.$store.dispatch("getTaskDetail", detailId).then(() => {
+        this.$router.push(`/tasks/${detailId}`);
+      });
+    },
+    deleteTask(deleteId){
+        this.$store.dispatch('deleteTask', deleteId).then(()=>{
+            this.$store.dispatch('getTasks')
+        })
+            // this.$store.dispatch()
+        
     }
-
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
