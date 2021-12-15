2009d350187859062a71d1eaf04d59d05d759f30
isanfelix <farisihsan95@gmail.com>
2021-12-15 11:27:34 +0700

    --Add vue init & firebase init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    
+    <navigation-bar></navigation-bar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './components/NavigationBar.vue'
+
+export default ({
+  name: "App",
+  components: { 
+    NavigationBar
+  },
+})
+</script>
+
+<style></style>
