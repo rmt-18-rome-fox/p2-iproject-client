e701cef3f56fcd53065fbef96ce62d2c6db83611
ingabd <hasanabdulrachman@gmail.com>
2021-12-15 23:31:30 +0700

    add container and canvas from sandbox

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,13 +1,108 @@
 <template>
-  
+  <div>
+    <canvas 
+      class="canvas-style"
+      :id="canvasId"
+      v-on:mousedown="mouseDown"
+    />
+  </div>
 </template>
 
 <script>
+import paper from 'paper'
+
 export default {
-  name: 'Canvas'
+  name: 'Canvas',
+  props: ['canvasId'],
+  data: () => ({
+    path: null,
+    scope: null
+  }),
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
+    },
+    chat: function (data) {
+      console.log('chat: ', data)
+    },
+    draw: function (data) {
+      // console.log('di receiver client', data)
+      this.drawer(data)
+    },
+    mouseDown: function () {
+      this.mouseHandler(true)
+    },
+    reset: function () {
+      this.reset()
+    }
+  },
+  methods: {
+    reset () {
+      this.scope.project.activeLayer.removeChildren()
+      this.$socket.emit('reset')
+    },
+    pathCreate (scope) {
+      scope.activate()
+      return new paper.Path({
+        strokeColor: '#000000',
+        strokeJoin: 'round',
+        strokeWidth: 1.5
+      })
+    },
+    createTool (scope) {
+      scope.activate()
+      return new paper.Tool()
+    },
+    mouseDown () {
+      this.tool = this.createTool(this.scope)
+      this.tool.onMouseDown = (event) => {
+        this.$socket.emit('mouseDown')
+        this.path = this.pathCreate(this.scope)
+        const x = event.point.x
+        const y = event.point.y
+        this.drawer({ x, y })
+        this.$socket.emit('draw', { x, y })
+      }
+      this.tool.onMouseDrag = (event) => {
+        const x = event.point.x
+        const y = event.point.y
+        this.drawer({ x, y })
+        this.$socket.emit('draw', { x, y })
+      }
+      this.tool.onMouseUp = (event) => {
+        const x = event.point.x
+        const y = event.point.y
+        this.drawer({ x, y })
+        this.$socket.emit('draw', { x, y })
+      }
+    },
+    drawer (data) {
+      this.path.add(data)
+    },
+    mouseHandler (data) {
+      if (data) {
+        this.path = this.pathCreate(this.scope)
+      } else {
+        this.path = null
+      }
+    }
+  },
+  mounted () {
+    this.scope = new paper.PaperScope()
+    this.scope.setup(this.canvasId)
+  }
 }
 </script>
 
 <style>
-
+.canvas-style {
+  cursor: crosshair;
+  width: 100% !important;
+  height: 500px !important;
+  border: 5px solid black;
+  border-radius: 10px;
+  display: block;
+  margin: auto;
+  box-shadow: 0 10px 8px -8px black;
+}
 </style>
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,10 +1,38 @@
 <template>
-  
+   <div>
+    <!-- button toggles -->
+    <div class="row mt-5">
+        <div class="col-5" />
+        <div class="col-2">
+            <button class="btn btn-primary btn-block" @click="reset">CLEAR</button>
+        </div>
+        <div class="col-5"/>
+    </div>
+
+    <!-- canvas single -->
+    <div class="row mt-5">
+        <div class="col-2"/>
+        <div class="col-8">
+            <Canvas :canvas-id="'canvas-one'" ref="childCanvas"/>
+        </div>
+        <div class="col-2"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import Canvas from '../components/Canvas.vue'
+
 export default {
-  name: 'Container'
+  name: 'Container',
+  components: {
+    Canvas
+  },
+  methods: {
+    reset: function () {
+      this.$refs.childCanvas.reset()
+    }
+  }
 }
 </script>
 
