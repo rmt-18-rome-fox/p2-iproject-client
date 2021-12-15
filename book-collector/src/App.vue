2eb1a8831a8b58a96c86014ee9372a532ec91eac
siska <siska25.oktaviani@gmail.com>
2021-12-15 11:40:25 +0700

    navbar template and link home and bookmark

--- book-collector/src/App.vue
+++ book-collector/src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components: { Navbar },
+  name: "App",
 
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
