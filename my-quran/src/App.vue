5a2940372b7c76406f901d4ed95e4080d12656aa
syswyndar <sysswyndar@gmail.com>
2021-12-15 22:01:20 +0700

    setup vue, add login and Navbar

---
+++ my-quran/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <login></login>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+export default {
+  components: {
+    Home,
+    Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
