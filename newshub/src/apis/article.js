0fb6aea7081921aa219fb480f05a8aaf991b4a22
muhano <geri.muhano@gmail.com>
2021-12-16 23:44:37 +0700

    revisi2

--- newshub/src/apis/article.js
+++ newshub/src/apis/article.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://server-newshub.herokuapp.com/";
 
 const instance = axios.create({
   baseURL: API_URL,
