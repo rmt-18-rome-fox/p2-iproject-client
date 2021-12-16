2e8499784a5c1c87d942f3fdb4cd1e15bf1a2f04
ihzaw <ihzanantama@gmail.com>
2021-12-16 21:05:36 +0800

    import vue-hacktiv8-footer

--- carbon-delivery/src/App.vue
+++ carbon-delivery/src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <Navbar />
     <router-view />
+    <HFooter class="" />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import HFooter from 'vue-hacktiv8-footer'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    HFooter
   },
   created() {
     if (localStorage.getItem('access_token')) {
