9f088cab2365c4a7a2c0cd1b4067fa9cf37eb643
muhano <geri.muhano@gmail.com>
2021-12-16 07:05:27 +0700

    add hacktiv8 footer

--- newshub/src/App.vue
+++ newshub/src/App.vue
@@ -6,15 +6,17 @@
     </div> -->
     <navbar></navbar>
     <router-view/>
+    <HFooter></HFooter>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import HFooter from 'vue-hacktiv8-footer'
 
 export default {
   name: "App",
-  components: {Navbar}
+  components: {Navbar, HFooter}
   
 }
 </script>
