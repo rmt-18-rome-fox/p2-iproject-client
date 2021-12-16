a75caa44c2bf331e1d4fe07cf99e4a1630b41cf7
michaelwijanto <michael.wijanto@gmail.com>
2021-12-16 21:13:46 +0700

    update login and register ui

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="flex items-center justify-center">
+  <div class="flex items-center justify-center min-h-screen">
       <div class="max-w-2xl mx-auto">
-	    <div class="bg-yellow-50 shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-		  <form class="space-y-6" @submit.prevent="register">
+	    <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm py-10 px-20">
+		  <form class="space-y-6 w-full flex flex-col justify-center items-center text-left" @submit.prevent="register">
 			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Register</h3>
 			<div>
-				<label for="email" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your email</label>
-				<input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" v-model="user.email">
+				<label for="email" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Email</label>
+				<input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 w-72 p-2.5" placeholder="name@company.com" v-model="user.email">
             </div>
             <div>
-                <label for="password" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Your password</label>
-                <input type="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" v-model="user.password">
+                <label for="password" class="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Password</label>
+                <input type="password" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:border-blue-500 w-72 p-2.5" v-model="user.password">
             </div>
-                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register your account</button>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-8 py-3 text-center">Register</button>
             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                 Already registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500" @click.prevent="toLogin">Login</a>
             </div>
