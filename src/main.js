d4114438ee51f6fd1e129661d6bb03c074d79cbd
glenn2206 <leonardusglenn@gmail.com>
2021-12-16 11:22:14 +0700

    deploy

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import VueSocketIOExt from 'vue-socket.io-extended'
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000')
+const socket = io('https://sewaja-mobil.herokuapp.com')
 
 Vue.use(VueSocketIOExt, socket, { store })
 
